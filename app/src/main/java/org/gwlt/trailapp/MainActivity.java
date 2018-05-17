b64a2139729401c0a4fe087f1dcb1a29c57e82f9
Chami Lamelas <sockz0101@gmail.com>
2018-05-17 16:20:07 +0100

    started report() and learnMore()

--- app/src/main/java/org/gwlt/trailapp/MainActivity.java
+++ app/src/main/java/org/gwlt/trailapp/MainActivity.java
@@ -1,7 +1,10 @@
 package org.gwlt.trailapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Generates an email in the mail client of the user's choice with the provided report message
+     * @param reportMsg - message to send in report to GWLT
+     */
+    public void report(String reportMsg) {
+        String recipients[] = {};
+        String reportTitle = "Report: " + Utilities.getFormattedTime("MM:dd:yyyy hh:mm:ss");
+        Intent emailIntent = Utilities.genEmail(recipients, reportTitle, reportMsg);
+
+        try {
+            // starts activity (opens mail app) based on user's choice from dialog created by createChooser()
+            startActivity(Intent.createChooser(emailIntent, "Choose mail client.. "));
+            finish(); // finishes activity
+        }
+        catch (android.content.ActivityNotFoundException ex) { // thrown by startActivity
+            Log.i("MainActivity:","no mail client installed.", ex); // use Log in place of System.out.println()
+        }
+    }
+
+    /**
+     * Lets user choose which web browser to open the link to GWLT's home page to learn more
+     */
+    public void learnMore() {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW); // specifies Intent is for viewing a URI
+        browserIntent.setData(Uri.parse("gwlt.org")); // sets data the Intent will work on
+
+        try {
+            // starts activity (opens browser) based on user's choice from dialog created by createChooser()
+            startActivity(Intent.createChooser(browserIntent, "Choose browser.. "));
+            finish();
+        }
+        catch (android.content.ActivityNotFoundException ex) { // thrown by startActivity
+            Log.i("MainActivity:", "no browser installed.", ex);
+        }
     }
 }
--- app/src/main/java/org/gwlt/trailapp/Utilities.java
+++ app/src/main/java/org/gwlt/trailapp/Utilities.java
@@ -2,32 +2,37 @@ package org.gwlt.trailapp;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.text.TextUtils;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URLEncoder;
-import java.util.ArrayList;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public final class Utilities {
 
-    public static void openEmailInDefaultMailApp(ArrayList<String> recipients, String subject, String body) {
-        try {
-            // String.join() added in Java 8; not supported in Android; use TextUtils.join() instead
-            String formattedRecipients = TextUtils.join(",", recipients);
-            String encodedSubject = URLEncoder.encode(subject, "UTF-8");
-            String encodedBody = URLEncoder.encode(body, "UTF-8");
-            // encode() replaces spaces with + signs, but for email URI format spaces should be replaced with %20
-            String formattedString = String.format("mailto:%s?subject=%s&body=%s", formattedRecipients, encodedSubject, encodedBody).replace("+", "%20");
-        }
-        catch (UnsupportedEncodingException uee) {
-            System.out.println("Encoding type not supported");
-        }
+    /**
+     * Generates an Intent that holds an email with the provided information:
+     * @param recipients - who to send the email to
+     * @param subject - subject of the email
+     * @param body - body of the email
+     * @return - email Intent that holds the data
+     */
+    public static Intent genEmail(String[] recipients, String subject, String body) {
+        Intent emailIntent = new Intent(Intent.ACTION_SEND); // specifies that the Intent is for email
+        emailIntent.setData(Uri.parse("mailto:")); // sets the type of data Intent will be handling
+        emailIntent.setType("text/plain"); // sets MIME type (just text, text w/ attachments, etc.)
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, recipients); // puts in recipients
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject); // puts in subject
+        emailIntent.putExtra(Intent.EXTRA_TEXT, body); // puts in body
+        return emailIntent;
     }
 
-    public static Intent openLinkInDefaultBrowser(String uri) {
-        return new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+    /**
+     * Gets the current time using a provided pattern
+     * @param pattern - e.g. "dd:MM:yyyy"
+     * @return - current time formatted to fit the provided pattern
+     */
+    public static String getFormattedTime(String pattern) {
+        DateFormat dateFormat = new SimpleDateFormat(pattern); // creates new date format using pattern
+        Calendar cal = Calendar.getInstance();
+        return dateFormat.format(cal.getTime()); // formats calendar data provided in previous line
     }
-
 }
